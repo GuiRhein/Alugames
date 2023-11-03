4e3891083246bd9ceaa7f819af5822ca1a3c7cf3
Guilherm Rhein <guilherme.rhein02@gmail.com>
2023-11-03 10:52:53 -0300

    Projeto Inicial

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function alterarStatus() {
+    var element = document.querySelector("#game-1");
+    element.classList.replace("dashboard__item__button", "dashboard__item__button--return");
+    console.log(element.classList);
+}
+
